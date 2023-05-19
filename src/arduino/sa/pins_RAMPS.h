528ab0c0835ca69cde9252252fc418184b7d9778
Sergey Loshchilov <sergei.nntu@gmail.com>
2023-05-19 11:41:00 +0300

    Actuators 4 and 5 added. Statistical Filtering of the angle applied. Calibration process optimised

--- src/arduino/sa/pins_RAMPS.h
+++ src/arduino/sa/pins_RAMPS.h
@@ -24,6 +24,10 @@
 #define Q_DIR_PIN          34
 #define Q_ENABLE_PIN       30
 
+#define W_STEP_PIN         16
+#define W_DIR_PIN          17
+#define W_ENABLE_PIN       23
+
 #define SDPOWER            -1
 #define SDSS               53
 #define LED_PIN            13
@@ -44,6 +48,9 @@
 #define Z_ENCODER_IN      A5
 #define E_ENCODER_IN     A12
 #define Q_ENCODER_IN     A10
+#define W_ENCODER_IN     A11
+
+#define G0_SERVO_PWM     11
 
 //
 // Misc. Functions
