9f0420bf646e5131c9cf9c1438ec666cb3bdc647
Maksim Kropanov <kropanov2012@gmail.com>
2023-10-09 08:48:24 +0300

    Add a new functionality
    - Create a background thread for running blockly code
    - Add the file "program.py" where blockly code is stored
    - Fix end points
     -- start_program - run background thread
     -- stop_program - stop thread
     -- get_program_state - return a id of executed block
     -- set_active_program - get the blockly code and insert it to the file
    - Add should_terminate_flag to stop executing thread
    - Move some logic to other functions for code repeating avoid
    - Add orm functions for executing blockly code

--- src/python/orm_http_server.py
+++ src/python/orm_http_server.py
@@ -12,6 +12,13 @@ from std_msgs.msg import Float32
 from moveit_commander import MoveGroupCommander
 from tf.transformations import quaternion_from_euler
 from flask import Flask, request, jsonify
+import threading
+import imp
+import program
+import textwrap
+import re
+
+program_thread = None
 
 moveit_commander.roscpp_initialize(sys.argv)
 group = MoveGroupCommander("arm")
@@ -22,7 +29,8 @@ group.set_max_acceleration_scaling_factor(1)
 app = Flask(__name__)
 
 pub = None
-source = None
+active_block_id = None
+should_terminate_flag = False
 
 def detect_tf(data):
     start = time.time()
@@ -68,6 +76,41 @@ def create_pose_message(x, y, z, pitch, roll, yaw):
 
     return pose_msg
 
+def set_pose(x, y, z, pitch, roll, yaw):
+    pose_msg = create_pose_message(x, y, z, pitch, roll, yaw)
+    group.set_pose_target(pose_msg)
+    group.set_planning_time(0.2)
+
+    success = group.go(wait=True)
+    return {"execute": success}
+
+def set_gripper_state(data):
+    gripper_state_msg = Float32()
+    gripper_state_msg.data = data
+
+    # Publish Gripper Pose
+    pub.publish(gripper_state_msg)
+
+    group.set_planning_time(0.1)
+    success = group.go(wait=True)
+    return {"execute": success}
+
+def set_active_block(id):
+    global active_block_id
+    active_block_id = id
+
+def orm_blockly_set_gripper_state(gripper_state):
+    return set_gripper_state(gripper_state)
+
+def orm_blockly_set_position(x, y, z, pitch, roll, yaw):
+    return set_pose(x, y, z, pitch, roll, yaw)
+
+def orm_blockly_delay(ms):
+    time.sleep(ms / 1000)
+
+def should_terminate():
+    return should_terminate_flag
+
 @app.route("/convert_pose", methods=["POST"])
 def convert_pose():
     data = request.json
@@ -79,29 +122,13 @@ def convert_pose():
     roll = data["roll"]
     yaw = data["yaw"]
 
-    pose_msg = create_pose_message(x, y, z, pitch, roll, yaw)
-
-    group.set_pose_target(pose_msg)
-
-    group.set_planning_time(0.2)
-
-    success = group.go(wait=True)
-
-    return {"execute": success}
-
+    return set_pose(x, y, z, pitch, roll, yaw)
+    
 @app.route("/set_gripper_state", methods=["POST"])
-def set_gripper_state():
+def gripper_state():
     data = request.json
-
-    gripper_state_msg = Float32()
-    gripper_state_msg.data = data["gripper"]
-
-    # Publish Gripper Pose
-    pub.publish(gripper_state_msg)
-
-    group.set_planning_time(0.1)
-    success = group.go(wait=True)
-    return {"execute": success}
+    gripper_state = data["gripper"]
+    return set_gripper_state(gripper_state)
 
 @app.route("/current_ip", methods=["GET"])
 def get_current_ip():
@@ -112,30 +139,57 @@ def get_current_ip():
 
 @app.route("/start_program", methods=["GET"])
 def start_program():
-    exec(source)
+    global program_thread
+    global should_terminate_flag
+
+    should_terminate_flag = False
+    if program_thread is None or not program_thread.is_alive():
+        imp.reload(program)
+        program_thread = threading.Thread(target=program.program_main, args=(should_terminate, set_active_block))
+        program_thread.start()
+        print("Program started!")
+    else:
+        print("Program is already running!")
 
 @app.route("/stop_program", methods=["GET"])
 def stop_program():
-    pass
+    global should_terminate_flag
+    if should_terminate_flag:
+        print("Program has been stopped!")
+    else:
+        should_terminate_flag = True
+        print("Program stopped!")
 
 @app.route("/get_active_program", methods=["GET"])
 def get_active_program():
     pass
 
+def insert_code(file_path, dynamic_code):
+    try:
+        code_with_indent = textwrap.indent(dynamic_code, '  ')
+        code = f"from orm_http_server import *\ndef program_main(should_terminate_function, set_active_block_id):\n{code_with_indent}"
+        
+        with open(file_path, 'w') as file:
+            file.write(code)
+
+    except Exception as e:
+        print(f"Error: {e}")
+
 @app.route("/set_active_program", methods=["POST"])
 def set_active_program():
     data = request.json
     source = data["source"]
+    insert_code('program.py', source)
 
 @app.route("/get_program_state", methods=["GET"])
 def get_program_state():
-    pass
+    global active_block_id
+    return {"id": active_block_id}
 
 def main():
     rospy.init_node('moveit_controller')
 
     global pub
-    global source
 
     pub = rospy.Publisher('/gripper_state', Float32, queue_size = 10)
     #rospy.spin()
@@ -147,4 +201,3 @@ if __name__ == '__main__':
         moveit_commander.roscpp_shutdown()
     except rospy.ROSInterruptException:
         print("Something went wrong!")
-
