df7529f9f0a46559c55d752c7e26608f49dcaa11
Maksim Kropanov <kropanov@gmail.com>
2024-12-23 15:21:30 +0300

    feat: draft blockly routes

--- server/api/routes/blockly/blockly.py
+++ server/api/routes/blockly/blockly.py
@@ -1,14 +1,24 @@
 from flask import Blueprint
 
+
 blockly = Blueprint('blockly', __name__)
 
 
-@blockly.route('/test', methods=('GET'))
-def test():
-    return 'This is a test route.'
+@blockly.route('/programs', methods=['GET'])
+def get_blockly_programs():
+    return 'Get active / non active program list'
 
-# TODO: release these routes
-# @blockly.route("/programs", methods=["GET"])
-# @blockly.route("/start", methods=["POST"])
-# @blockly.route("/stop", methods=["POST"])
 
+@blockly.route('/start', methods=['POST'])
+def start_blockly_program():
+    return 'Start selected program'
+
+
+@blockly.route('/stop', methods=['POST'])
+def stop_blockly_program():
+    return 'Stop selected program'
+
+
+@blockly.route('/save', methods=['POST'])
+def save_blockly_program():
+    return 'Save selected program'
