955164797d5033f75735a6e6b23f016915fb0e36
Maksim Kropanov <kropanov@gmail.com>
2025-01-21 09:59:28 +0300

    feat(blockly): add catching errors in get_active_program()

--- server/api/routes/blockly/get_active_program.py
+++ server/api/routes/blockly/get_active_program.py
@@ -1,2 +1,16 @@
 def get_active_program():
-    print('get_active_program')
+    try:
+        print('get_active_program')
+
+        response = JsonResponse(status='success', data={}, message='Request processed successfully')
+        return response.to_json(200)
+
+    except KeyError as e:
+                error_message = f"Data access error: {str(e)}"
+                response = JsonResponse(status="error", message="Data access error", error=error_message)
+                return response.to_json(500)
+
+    except Exception as e:
+        error_message = f"An unexpected error occurred: {str(e)}"
+        response = JsonResponse(status="error", message="Unexpected error", error=error_message)
+        return response.to_json(500)
