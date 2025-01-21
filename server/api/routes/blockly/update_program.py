7548a64c053895986e64a024f2fcc08448c6b5db
Maksim Kropanov <kropanov@gmail.com>
2025-01-21 10:17:10 +0300

    feat(blockly): add catching errors in update_program()

--- server/api/routes/blockly/update_program.py
+++ server/api/routes/blockly/update_program.py
@@ -5,19 +5,32 @@ from dao import BlocklyDAO
 
 
 def update_program():
-    data = request.get_json()
-    id = request.args.get('id')
+    try:
+        data = request.get_json()
+        id = request.args.get('id')
 
-    program_data = {
-        'name': data.get('program_name'),
-        'description': data.get('program_description'),
-        'structure': data.get('program_structure'),
-        'is_running': data.get('is_running'),
-    }
+        # TODO: add validation
+        program_data = {
+            'name': data.get('program_name'),
+            'description': data.get('program_description'),
+            'structure': data.get('program_structure'),
+            'is_running': data.get('is_running'),
+        }
 
-    program = SimpleNamespace(**program_data)
+        program = SimpleNamespace(**program_data)
 
-    dao = BlocklyDAO()
-    dao.update_program(program, id)
+        dao = BlocklyDAO()
+        dao.update_program(program, id)
 
-    return 'program'
+        response = JsonResponse(status='success', data={}, message='Request processed successfully')
+        return response.to_json(200)
+
+    except KeyError as e:
+        error_message = f"Data access error: {str(e)}"
+        response = JsonResponse(status="error", message="Data access error", error=error_message)
+        return response.to_json(500)
+
+    except Exception as e:
+        error_message = f"An unexpected error occurred: {str(e)}"
+        response = JsonResponse(status="error", message="Unexpected error", error=error_message)
+        return response.to_json(500)
