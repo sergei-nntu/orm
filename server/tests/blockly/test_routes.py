085328e17c14b3be80f42c3c4f3b442627095fe2
Maksim Kropanov <kropanov@gmail.com>
2025-01-27 11:22:31 +0300

    docs: comment test functionality

--- server/tests/blockly/test_routes.py
+++ server/tests/blockly/test_routes.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 
 def test_start_program(client):
-    """Test the /blockly/start route."""
+    """Test the POST /blockly/start route."""
     base_dir = os.path.dirname(os.path.abspath(__file__))
     test_data_dir = os.path.join(base_dir, 'test_data')
     program_file_path = os.path.join(test_data_dir, 'program.txt')
@@ -28,7 +28,7 @@ def test_start_program(client):
     assert response_data['message'] == 'Program started successfully.'
 
 def test_start_program_when_already_running(client):
-    """Test the /blockly/start route."""
+    """Test the POST /blockly/start route with already running program"""
     base_dir = os.path.dirname(os.path.abspath(__file__))
     test_data_dir = os.path.join(base_dir, 'test_data')
     program_file_path = os.path.join(test_data_dir, 'program.txt')
@@ -55,6 +55,7 @@ def test_start_program_when_already_running(client):
     assert response_data['message'] == 'Program is already running.'
 
 def test_start_program_without_program(client):
+    """ Test the POST /blockly/start route without any program"""
     base_dir = os.path.dirname(os.path.abspath(__file__))
     test_data_dir = os.path.join(base_dir, 'test_data')
     structure_file_path = os.path.join(test_data_dir, 'structure.json')
@@ -74,6 +75,7 @@ def test_start_program_without_program(client):
     assert response_data['data'] is None
 
 def test_start_program_without_structure(client):
+    """ Test the POST /blockly/start route without any structure"""
     base_dir = os.path.dirname(os.path.abspath(__file__))
     test_data_dir = os.path.join(base_dir, 'test_data')
     program_file_path = os.path.join(test_data_dir, 'program.txt')
@@ -93,6 +95,7 @@ def test_start_program_without_structure(client):
     assert response_data['data'] is None
 
 def test_start_program_without_params(client):
+    """ Test the POST /blockly/start route without program and structure"""
     response = client.post('/blockly/start', json={})
     response_data = response.json
 
@@ -104,6 +107,7 @@ def test_start_program_without_params(client):
     assert response_data['data'] is None
 
 def test_get_program_with_invalid_id(client):
+    """ Test the GET /blockly/<int:id> route with invalid id"""
     response = client.get('/blockly/99999')
     response_data = response.json
 
@@ -115,7 +119,7 @@ def test_get_program_with_invalid_id(client):
     assert response_data['data'] is None
 
 def test_stop_program(client):
-    """Test stop program route."""
+    """Test the POST /blockly/stop route"""
     response = client.post('/blockly/stop')
     response_data = response.json
 
@@ -125,6 +129,7 @@ def test_stop_program(client):
     assert response_data['status'] == 'success'
 
 def test_delete_program_with_nonexistent_id(client):
+    """Test the DELETE /blockly route with nonexistent id"""
     unique_id = 99999
     query_params = {"id": unique_id}
 
@@ -145,6 +150,7 @@ def test_delete_program_with_nonexistent_id(client):
     assert response_data['message'] == 'No program found with the provided ID'
 
 def test_get_programs(client):
+    """Test the GET /blockly route"""
     response = client.get('/blockly')
     response_data = response.json
 
@@ -153,6 +159,7 @@ def test_get_programs(client):
     assert response.status_code == 200
 
 def test_save_program(client):
+    """Test the POST /blockly route"""
     body = {
         'program_name': 'Test program name' + str(datetime.now()),
         'program_description': 'Test program description',
@@ -168,6 +175,7 @@ def test_save_program(client):
     assert response_data['status'] == 'success'
 
 def test_save_program_without_body_params(client):
+    """Test the POST /blockly route without body params"""
     body = {
         'program_description': 'Test program description',
         'program_structure': 'Test program structure',
@@ -205,6 +213,7 @@ def test_save_program_without_body_params(client):
     assert response_data['message'] == "Both 'program_name', 'program_description' and 'program_structure' must be provided in the request"
 
 def test_update_program(client):
+    """Test the PATCH /blockly route"""
     query_params = {"id": "0"}
 
     body = {
@@ -221,6 +230,7 @@ def test_update_program(client):
     assert response.status_code == 200
 
 def test_update_program_without_id(client):
+    """Test the PATCH /blockly route"""
     body = {
         'program_name': 'Updated program_name' + str(datetime.now()),
         'program_description': 'Updated program_description',
@@ -237,6 +247,7 @@ def test_update_program_without_id(client):
     assert response_data['message'] == "The program id should be provided as a query parameter - /blockly?id=0"
 
 def test_update_program_without_name(client):
+    """Test the PATCH /blockly route without name"""
     query_params = {"id": "0"}
 
     body = {
@@ -254,6 +265,7 @@ def test_update_program_without_name(client):
     assert response_data['message'] == "Both 'program_name', 'program_description' and 'program_structure' must be provided in the request"
 
 def test_update_program_without_description(client):
+    """Test the PATCH /blockly route without description"""
     query_params = {"id": "0"}
 
     body = {
@@ -271,6 +283,7 @@ def test_update_program_without_description(client):
     assert response_data['message'] == "Both 'program_name', 'program_description' and 'program_structure' must be provided in the request"
 
 def test_update_program_without_structure(client):
+    """Test the PATCH /blockly route without structure"""
     query_params = {"id": "0"}
 
     body = {
@@ -288,6 +301,7 @@ def test_update_program_without_structure(client):
     assert response_data['message'] == "Both 'program_name', 'program_description' and 'program_structure' must be provided in the request"
 
 def test_get_current_program(client):
+    """Test the GET /blockly/current-program route"""
     response = client.get('blockly/current-program')
     response_data = response.json
 
@@ -298,6 +312,7 @@ def test_get_current_program(client):
     assert response_data['data'] != {}
 
 def test_get_current_block_id(client):
+    """Test the GET /blockly/current-block-id route"""
     response = client.get('blockly/current-block-id')
     response_data = response.json
 
